8547e1a4dbc3a815a7b38ad46936e1f9bff7b71d
eviljay <evgencherkashyn@gmail.com>
2021-11-03 19:39:12 +0200

    5564

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+
+    if(height > 450) {
+    	$('#content-dcuo .container').addClass('animated fadeInUp show').removeClass('hidden fadeOutUp');
+    }
+});
+
+$( document ).ready(function() {
+	
+
+});
